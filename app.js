8f6a0c3fecbe14f571b35025fc7f2920fedb01f9
Chris Rehagen <ccrwy5@mail.missouri.edu>
2020-06-21 12:40:20 -0500

    Add dynamic date change/load

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ searchButton.addEventListener("click", ()=>{
 
     let str = new Date(dateBox.value).toISOString();
     var rawDate = str.substring(0, 10);
-    console.log(rawDate);
-
 
     sendApiRequest(rawDate)
 })
@@ -22,7 +20,7 @@ async function sendApiRequest(date){
     let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&start_date=${date}&end_date=${date}`);
     
     let data = await response.json();
-    console.log(data[0]);
+    //console.log(data[0]);
     useApiData(data[0]);
 }
 
@@ -33,14 +31,15 @@ function useApiData(data){
 
     var date = setDate(data);
 
-    document.querySelector("#date").innerHTML += date;
-    document.querySelector("#image").innerHTML += `<img src="${data.url}">`;
-    document.querySelector("#content").innerHTML += data.explanation;
+    document.querySelector("#date").innerHTML = date;
+    document.querySelector("#image").innerHTML = `<img src="${data.url}">`;
+    document.querySelector("#content").innerHTML = data.explanation;
 
 
 }
 
 function setDate(data){
+
     var d = new Date(data.date);
     var year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
     var month = new Intl.DateTimeFormat('en', { month: 'long' }).format(d);
@@ -48,4 +47,16 @@ function setDate(data){
     
     var formattedDate = (`${month} ${day}, ${year}`);
     return formattedDate;
+}
+
+function setToday(){
+
+    var today = new Date();
+    var todayDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+    //console.log(todayDate);
+
+
+
+    sendApiRequest(todayDate);
 }
